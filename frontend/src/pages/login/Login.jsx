8b3758bf1930775bb2ed66564036b103f51b1243
aniasusual <82938725+aniasusual@users.noreply.github.com>
2023-06-22 19:12:42 +0530

    .

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,76 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+import sample from '../../assets/sample.mp4';
+
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/")
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  return (
+    <div className="login">
+
+    <video className='videoTag' autoPlay loop muted>
+      <source src={sample} type='video/mp4' />
+    </video>
+
+      <div className="card">
+        <div className="left">
+          <h1>Hello JUITians</h1>
+          <p>
+            This is a platform to connect you with all your college mates and alumini.
+            The vision is to create more oppurtunities by elimination barriers of communication.
+          <div><span style={{color: "red"}}>This project is developed by ANIMESH 201567</span></div>
+
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
